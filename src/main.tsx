02a9b67d68a49d68b046a64f76fa97313c5183e2
AlbertoV5 <avq5ac1@gmail.com>
2023-09-06 19:31:05 -0600

    404a

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './404.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
+  {
+    path: '/*',
+    element: <NotFound/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
