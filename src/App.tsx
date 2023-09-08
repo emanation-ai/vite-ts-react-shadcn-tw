7e4546e01ca4adee444df13f87bbab71ca160628
AlbertoV5 <avq5ac1@gmail.com>
2023-09-08 12:31:00 -0600

    added data

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
-import { UserAuthForm } from '@/components/app/user-auth-form';
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { UserAuthForm } from "@/components/app/user-auth-form";
+import { branding, login, signup } from "./data/main_data.json";
 
 function App() {
   return (
     <>
-      <div className="container relative h-[800px] flex-col items-center justify-center pt-48 md:pt-0 md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-screen flex-col items-center justify-center pt-48 md:pt-0 md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
-          to="/examples/authentication"
+          to={login.login_button_link}
           className={cn(
-            buttonVariants({ variant: 'ghost' }),
-            'absolute right-4 top-4 md:right-8 md:top-8'
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
           )}
         >
-          Login
+          {login.login_button_label}
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
@@ -32,39 +33,37 @@ function App() {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            Acme Inc
+            {branding.company_name}
           </div>
           <div className="relative z-20 mt-auto">
-            {/* <blockquote className="space-y-2">
+            <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                &ldquo;{branding.testimony_quote}&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote> */}
+              <footer className="text-sm">{branding.testimony_author}</footer>
+            </blockquote>
           </div>
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 ">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                {signup.signup_title}
               </h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                {signup.signup_subtitle}
               </p>
             </div>
             <UserAuthForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{' '}
+              By clicking continue, you agree to our{" "}
               <Link
                 to="/terms"
                 className="underline underline-offset-4 hover:text-primary"
               >
                 Terms of Service
-              </Link>{' '}
-              and{' '}
+              </Link>{" "}
+              and{" "}
               <Link
                 to="/privacy"
                 className="underline underline-offset-4 hover:text-primary"
