2420a2358bbd6439d169fd1a1561576587184138
waleedalikhan <waleedomore@gmail.com>
2023-10-04 22:29:27 +0500

    added stripe api for demo purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,109 @@
-import { useContext } from 'react'
-import { HomePageContext, IGlobalDataType } from '@/context/HomeContext'
-import { NavigationMenuDemo } from '@/components/Nav'
+import React, { useEffect, useState } from 'react'
+import Stripe from 'stripe'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
-function App() {
-  const appData = useContext<IGlobalDataType>(HomePageContext)
+const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  let [balanceTrans, setBalanceTrans] = useState<Stripe.BalanceTransaction[]>(
+    [],
+  )
+  const stripe = new Stripe(
+    'sk_test_51ANTqzFuMobrpxHlxHbI9cO77ROzylZe9XKItVqkUGmp8yrhWt0mqVcUMZ1dUlDwvV4Mb2x6ng51JS9Bin2dIKQk00ulhsURc2',
+    {
+      apiVersion: '2023-08-16',
+    },
+  )
+
+  const getBalanceTransactions = async () => {
+    setIsLoading(true)
+    const balanceTransactions = await stripe.balanceTransactions.list()
+    setBalanceTrans((balanceTrans = balanceTransactions.data))
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getBalanceTransactions()
+  }, [])
 
   return (
-    <main>
-      <div className="bg-white text-black">
-        <nav
-          className="flex items-center justify-center pt-6 lg:px-8"
-          aria-label="Global"
-        >
-          <NavigationMenuDemo buttons={appData.header_navigation} />
-        </nav>
-        <div className="relative isolate px-6 pt-14 lg:px-8">
-          <div
-            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-            aria-hidden="true"
-          ></div>
-          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-            <div className="text-center">
-              <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
-                {appData.hero_banner_title}
-              </h1>
-              <p className="mt-6 text-lg leading-8">
-                {appData.hero_banner_desc}
-              </p>
-              <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md bg-white border px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  {appData.hero_primary_call_to_action}
-                </a>
-                <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 px-3.5 py-2.5"
-                >
-                  {appData.hero_secondary_call_to_action}{' '}
-                  <span aria-hidden="true">→</span>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-            aria-hidden="true"
-          ></div>
-        </div>
-      </div>
-      <footer className="bg-white dark:bg-gray-900 w-100">
-        <div className="mx-auto w-full max-w-screen-xl">
-          <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
-            {appData.footer_navigation.links.map((c) => (
-              <div key={c.title}>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                  {c.title}
-                </h2>
-                <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                  {c.items.map((i) => (
-                    <li key={i.title} className="mb-4">
-                      <a href={i.href} className=" hover:underline">
-                        {i.title}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="px-4 py-6 bg-gray-100 dark:bg-gray-700 w-100">
-          <div className="flex mt-4 space-x-5 sm:justify-center md:mt-0">
-            <a
-              href={appData.footer_navigation.copyRights.href}
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
+    <main className="p-10">
+      {isLoading && balanceTrans.length === 0 ? (
+        <>
+          <svg
+            id="infinite-indicator"
+            width="200"
+            height="200"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              r="40"
+              cx="50"
+              cy="50"
+              fill="none"
+              strokeWidth={10}
+              stroke="#000"
+              strokeOpacity={0.6}
+            ></circle>
+            <path
+              d="M10 50 A40 40 0 1 1 90 50"
+              fill="none"
+              stroke="#000"
+              strokeWidth={10}
             >
-              {appData.footer_navigation.copyRights.title}
-            </a>
-          </div>
-        </div>
-      </footer>
+              <animateTransform
+                attributeType="xml"
+                attributeName="transform"
+                type="rotate"
+                from="0 50 50"
+                to="360 50 50"
+                dur="1s"
+                repeatCount="indefinite"
+              />
+            </path>
+          </svg>
+        </>
+      ) : (
+        <Table>
+          <TableCaption>A list of your balance transactions.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Invoice</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>status</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {balanceTrans.map((transaction) => {
+              return (
+                <React.Fragment key={transaction.id}>
+                  <TableRow>
+                    <TableCell className="font-medium capitalize">
+                      {transaction.object.split('_').join(' ')}
+                    </TableCell>
+                    <TableCell>{transaction.description}</TableCell>
+                    <TableCell className="capitalize">
+                      {transaction.status}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      ${transaction.amount}
+                    </TableCell>
+                  </TableRow>
+                </React.Fragment>
+              )
+            })}
+          </TableBody>
+        </Table>
+      )}
     </main>
   )
 }
